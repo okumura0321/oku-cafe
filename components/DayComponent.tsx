68fc99409439090140a24552011de9cd46e44e27
okumura <okumura0321@gmail.com>
2025-01-30 02:08:30 +0900

    [skip ci]全体的にブラッシュアップ

--- components/DayComponent.tsx
+++ components/DayComponent.tsx
@@ -1,14 +1,20 @@
-import React, { FC } from 'react';
-import { Box, Typography, Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import Image from 'next/image';
-import { Kiwi_Maru } from 'next/font/google';
+import React, { FC } from "react";
+import {
+  Box,
+  Typography,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+} from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import Image from "next/image";
+import { Kiwi_Maru } from "next/font/google";
+import { KeyboardArrowDown } from "@mui/icons-material";
 
 const kiwiMaru = Kiwi_Maru({
-  weight: ['400', '500'],
-  subsets: ['latin'],
-  display: 'swap',
+  weight: ["400", "500"],
+  subsets: ["latin"],
+  display: "swap",
 });
 
 const dayTheme = createTheme({
@@ -17,27 +23,53 @@ const dayTheme = createTheme({
   },
   palette: {
     background: {
-      default: '#FFFBCC',
+      default: "#FFFBCC",
     },
     text: {
-      primary: '#333333',
+      primary: "#333333",
     },
   },
 });
 
 const menuItems = [
-  { category: 'Drinks', items: [
-    { name: 'コーヒー', image: '/images/hot-coffee.jpg', description: '・ホット\n・アイス\n・カフェオレ' },
-    { name: '紅茶', image: '/images/hot-tea.jpg', description: '・ホット\n・アイス' },
-    { name: 'ﾌﾚｰﾊﾞｰﾃｨｰ', image: '/images/flavor.jpg', description: '・レモンティー\n・はちみつ\n' },
-    { name: 'お茶', image: '/images/tea.jpg', description: '・緑茶\n・麦茶' },
-  ]},
-  { category: 'Kids Menu', items: [
-    { name: 'お水', image: '/images/water.png', description: '' },
-    { name: 'お茶', image: '/images/mugitea.jpg', description: '・麦茶' },
-    { name: 'ジュース', image: '/images/orange-juice.jpg', description: '・オレンジ\n・りんご\n・ぶどう' },
-    { name: 'ミルク', image: '/images/milk.png', description: '・アイス\n・ホット' },
-  ]}
+  {
+    category: "Drinks",
+    items: [
+      {
+        name: "コーヒー",
+        image: "/images/hot-coffee.jpg",
+        description: "・ホット\n・アイス\n・カフェオレ",
+      },
+      {
+        name: "紅茶",
+        image: "/images/hot-tea.jpg",
+        description: "・ホット\n・アイス",
+      },
+      {
+        name: "ﾌﾚｰﾊﾞｰﾃｨｰ",
+        image: "/images/flavor.jpg",
+        description: "・レモンティー\n・はちみつ\n",
+      },
+      { name: "お茶", image: "/images/tea.jpg", description: "・緑茶\n・麦茶" },
+    ],
+  },
+  {
+    category: "Kids Menu",
+    items: [
+      { name: "お水", image: "/images/water.png", description: "" },
+      { name: "お茶", image: "/images/mugitea.jpg", description: "・麦茶" },
+      {
+        name: "ジュース",
+        image: "/images/orange-juice.jpg",
+        description: "・オレンジ\n・りんご\n・ぶどう",
+      },
+      {
+        name: "ミルク",
+        image: "/images/milk.png",
+        description: "・アイス\n・ホット",
+      },
+    ],
+  },
 ];
 
 const DayComponent: FC = () => (
@@ -47,28 +79,89 @@ const DayComponent: FC = () => (
         sx={{
           padding: 4,
           backgroundColor: dayTheme.palette.background.default,
-          minHeight: '100vh',
+          minHeight: "100vh",
           color: dayTheme.palette.text.primary,
         }}
       >
+        <Box sx={{ textAlign: "left", mb: 4 }}>
+          <Typography
+            variant="h3"
+            sx={{
+              fontWeight: "bold",
+              borderBottom: "3px solid black",
+              paddingBottom: "10px",
+            }}
+          >
+            昼のメニュー
+          </Typography>
+          <Typography variant="body1" sx={{ mt: 2, fontSize: "1.2rem" }}>
+            コーヒーや紅茶など、お昼のひとときを楽しむドリンクをご用意しています。
+          </Typography>
+        </Box>
+
         {menuItems.map((menu) => (
-          <Accordion key={menu.category}  sx={{ backgroundColor: dayTheme.palette.background.default }}>
-            <AccordionSummary expandIcon={<ExpandMoreIcon />}> 
-              <Typography variant="h4">{menu.category}</Typography>
+          <Accordion
+            key={menu.category}
+            sx={{ backgroundColor: dayTheme.palette.background.default }}
+          >
+            <AccordionSummary expandIcon={<KeyboardArrowDown sx={{ color: 'brown', fontSize: 30 }} />}            >
+              <Typography
+                variant="h4"
+                sx={{
+                  fontWeight: "bold",
+                  borderBottom: "2px solid black",
+                  paddingBottom: "4px",
+                }}
+              >
+                {menu.category}
+              </Typography>
             </AccordionSummary>
-            <AccordionDetails sx={{ backgroundColor: dayTheme.palette.background.default }}>
-              <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 2, justifyContent: 'center', maxWidth: '1000px', margin: '0 auto' }}>
+            <AccordionDetails
+              sx={{ backgroundColor: dayTheme.palette.background.default }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  flexWrap: "wrap",
+                  gap: 2,
+                  justifyContent: "center",
+                  maxWidth: "1000px",
+                  margin: "0 auto",
+                }}
+              >
                 {menu.items.map((item) => (
-                  <Box key={item.name} sx={{ width: { xs: '100%', sm: '48%', md: '30%' }, display: 'flex', gap: 2, alignItems: 'left' }}>
-                    <Box sx={{ display: 'flex', gap: 2, alignItems: 'left' }}>
+                  <Box
+                    key={item.name}
+                    sx={{
+                      width: { xs: "100%", sm: "48%", md: "30%" },
+                      display: "flex",
+                      gap: 2,
+                      alignItems: "left",
+                    }}
+                  >
+                    <Box sx={{ display: "flex", gap: 2, alignItems: "left" }}>
                       <Box sx={{ flexShrink: 0 }}>
-                        <Image src={item.image} alt={item.name} width={160} height={160} style={{ borderRadius: '8px' }} />
+                        <Image
+                          src={item.image}
+                          alt={item.name}
+                          width={150}
+                          height={150}
+                          style={{ borderRadius: "8px" }}
+                        />
                       </Box>
                       <Box>
-                        <Typography variant="h5" gutterBottom sx={{ textAlign: 'left', fontWeight: 550 }}>
+                        <Typography
+                          variant="h5"
+                          gutterBottom
+                          sx={{ textAlign: "left", fontWeight: 550 }}
+                        >
                           {item.name}
                         </Typography>
-                        <Typography variant="body1" color="textSecondary" sx={{ whiteSpace: 'pre-line', textAlign: 'left' }}>
+                        <Typography
+                          variant="body1"
+                          color="textSecondary"
+                          sx={{ whiteSpace: "pre-line", textAlign: "left" }}
+                        >
                           {item.description}
                         </Typography>
                       </Box>
--- components/NightComponent.tsx
+++ components/NightComponent.tsx
@@ -1,75 +1,202 @@
-import React, { FC } from 'react';
-import { Box, Typography, Grid } from '@mui/material';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import Image from 'next/image';
+import React, { FC } from "react";
+import {
+  Box,
+  Typography,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+} from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import Image from "next/image";
+import { Kiwi_Maru } from "next/font/google";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+
+const kiwiMaru = Kiwi_Maru({
+  weight: ["400", "500"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 const nightTheme = createTheme({
+  typography: {
+    fontFamily: '"Kiwi Maru", serif',
+  },
   palette: {
     background: {
-      default: '#1E1E2F',
+      default: "#1E1E2F",
     },
     text: {
-      primary: '#FFFFFF',
+      primary: "#FFFFFF",
     },
   },
 });
 
 const nightMenuItems = [
-  { category: 'Beer', items: [
-      { name: 'キリン', image: '/images/kirin.png', description: '' },
-      { name: 'アサヒ', image: '/images/asahi.jpg', description: '' },
-    ]},
-  { category: 'Cocktail', items: [
-      { name: 'ジン', image: '/images/gin.jpg', description: '' },
-      { name: 'ウオッカ', image: '/images/vodka.jpg', description: '' },
-      { name: 'ラム', image: '/images/rum.jpg', description: '' },
-      { name: 'ディタ', image: '/images/ditta.jpg', description: '' },
-      { name: 'カシス', image: '/images/cassis.jpg', description: '' },
-      { name: 'ミスティア', image: '/images/mistia.jpg', description: '' },
-    ]},
-  { category: 'Whisky', items: [
-      { name: 'ロック', image: '/images/whisky.jpg', description: '' },
-      { name: 'ハイボール・ジンジャーハイボール', image: '/images/highball.png', description: '' },
-    ]},
+  {
+    category: "Beer",
+    items: [
+      { name: "キリン", image: "/images/kirin.png", description: "・一番搾り" },
+      {
+        name: "アサヒ",
+        image: "/images/asahi.jpg",
+        description: "・ｽｰﾊﾟｰﾄﾞﾗｲ",
+      },
+    ],
+  },
+  {
+    category: "Cocktail",
+    items: [
+      {
+        name: "Gin",
+        image: "/images/gin.jpg",
+        description: "・ｼﾞﾝﾄﾆｯｸ\n・ｼﾞﾝﾗｲﾑ\n・ｼﾞﾝﾘｯｷｰ",
+      },
+      {
+        name: "Vodka",
+        image: "/images/vodka.jpg",
+        description: "・ｳｫｯｶﾄﾆｯｸ\n・ﾓｽｺﾐｭｰﾙ\n・ｽｸﾘｭｰﾄﾞﾗｲﾊﾞｰ",
+      },
+      {
+        name: "Rum",
+        image: "/images/rum.jpg",
+        description: "・ﾗﾑｺｰｸ\n・ﾗﾑﾄﾆｯｸ\n",
+      },
+      { name: "Dita", image: "/images/dita.jpg", description: "・ﾃﾞｨﾀﾄﾆｯｸ" },
+      {
+        name: "Cassis",
+        image: "/images/cassis.jpg",
+        description: "・ｶｼｽｵﾚﾝｼﾞ\n・ｶｼｽｿｰﾀﾞ",
+      },
+      {
+        name: "Mistia",
+        image: "/images/mistia.jpg",
+        description: "・ﾐｽﾃｨｱｿｰﾀﾞ",
+      },
+      {
+        name: "Beer",
+        image: "/images/shandygaff.jpg",
+        description: "・ｼｬﾝﾃﾞｨｶﾞﾌ\n・ｺｰｸﾋﾞｱ\n・ﾊﾟﾅｼｪ\n・ｶｼｽﾋﾞｱ",
+      },
+    ],
+  },
+  {
+    category: "Whisky",
+    items: [
+      {
+        name: "MACALLAN",
+        image: "/images/macallan.jpg",
+        description: "・ﾛｯｸ\n・ﾊｲﾎﾞｰﾙ",
+      },
+      {
+        name: "角",
+        image: "/images/kaku.jpg",
+        description: "・ﾛｯｸ\n・ﾊｲﾎﾞｰﾙ\n・ｼﾞﾝｼﾞｬｰﾊｲﾎﾞｰﾙ",
+      },
+    ],
+  },
 ];
 
 const NightComponent: FC = () => (
   <ThemeProvider theme={nightTheme}>
-    <Box
-      sx={{
-        padding: 4,
-        backgroundColor: nightTheme.palette.background.default,
-        minHeight: '100vh',
-        color: nightTheme.palette.text.primary,
-      }}
-    >
-      {nightMenuItems.map((menu) => (
-        <Box key={menu.category} sx={{ marginBottom: 6 }}>
-          <Typography variant="h3" gutterBottom>
-            {menu.category}
+    <div className={kiwiMaru.className}>
+      <Box
+        sx={{
+          padding: 4,
+          backgroundColor: nightTheme.palette.background.default,
+          minHeight: "100vh",
+          color: nightTheme.palette.text.primary,
+        }}
+      >
+        <Box sx={{ textAlign: "left", mb: 4 }}>
+          <Typography
+            variant="h3"
+            sx={{
+              fontWeight: "bold",
+              borderBottom: "3px solid white",
+              paddingBottom: "10px",
+            }}
+          >
+            夜のメニュー
           </Typography>
-          <Grid container spacing={4}>
-            {menu.items.map((item) => (
-              <Grid item xs={12} sm={6} key={item.name}>
-                <Box sx={{ display: 'flex', gap: 2, alignItems: 'center' }}>
-                  <Box sx={{ flexShrink: 0 }}>
-                    <Image src={item.image} alt={item.name} width={120} height={120} style={{ borderRadius: '8px' }} />
-                  </Box>
-                  <Box>
-                    <Typography variant="h6" gutterBottom sx={{ textAlign: 'center' }}>
-                      {item.name}
-                    </Typography>
-                    <Typography variant="body1" color="textSecondary" sx={{ whiteSpace: 'pre-line', textAlign: 'left' }}>
-                      {item.description}
-                    </Typography>
-                  </Box>
-                </Box>
-              </Grid>
-            ))}
-          </Grid>
+          <Typography variant="body1" sx={{ mt: 2, fontSize: "1.2rem" }}>
+            カクテルやウィスキーなど、特別な時間を彩るドリンクをご用意しています。
+          </Typography>
         </Box>
-      ))}
-    </Box>
+
+        {nightMenuItems.map((menu) => (
+          <Accordion
+            key={menu.category}
+            sx={{ backgroundColor: nightTheme.palette.background.default }}
+          >
+            <AccordionSummary expandIcon={<KeyboardArrowDownIcon sx={{ color: 'gold', fontSize: 30 }} />}>
+              <Typography
+                variant="h4"
+                sx={{
+                  fontWeight: "bold",
+                  borderBottom: "2px solid white",
+                  paddingBottom: "4px",
+                }}
+              >
+                {menu.category}
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails
+              sx={{ backgroundColor: nightTheme.palette.background.default }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  flexWrap: "wrap",
+                  gap: 2,
+                  justifyContent: "center",
+                  maxWidth: "1000px",
+                  margin: "0 auto",
+                }}
+              >
+                {menu.items.map((item) => (
+                  <Box
+                    key={item.name}
+                    sx={{
+                      width: { xs: "100%", sm: "48%", md: "30%" },
+                      display: "flex",
+                      gap: 2,
+                      alignItems: "left",
+                    }}
+                  >
+                    <Box sx={{ display: "flex", gap: 2, alignItems: "left" }}>
+                      <Box sx={{ flexShrink: 0 }}>
+                        <Image
+                          src={item.image}
+                          alt={item.name}
+                          width={140}
+                          height={140}
+                          style={{ borderRadius: "8px" }}
+                        />
+                      </Box>
+                      <Box>
+                        <Typography
+                          variant="h5"
+                          gutterBottom
+                          sx={{ textAlign: "left", fontWeight: 550 }}
+                        >
+                          {item.name}
+                        </Typography>
+                        <Typography
+                          variant="body1"
+                          sx={{ whiteSpace: "pre-line", textAlign: "left" }}
+                        >
+                          {item.description}
+                        </Typography>
+                      </Box>
+                    </Box>
+                  </Box>
+                ))}
+              </Box>
+            </AccordionDetails>
+          </Accordion>
+        ))}
+      </Box>
+    </div>
   </ThemeProvider>
 );
 
